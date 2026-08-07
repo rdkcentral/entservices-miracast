c05fad72e6ffd3ce01d79e803e84256f0ce6bb37
srinibas15 <113517102+srinibas15@users.noreply.github.com>
2026-08-07 23:59:05 +0530

    RDKEMW-22169:Update build_dependencies.sh

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -29,12 +29,12 @@ cd ..
 # Clone the required repositories
 
 
-git clone -b R4_4-RDK https://github.com/rdkcentral/ThunderTools.git
+git clone --branch  R4_4-RDK https://github.com/rdkcentral/ThunderTools.git
 cd ThunderTools
 git checkout $THUNDER_TOOLS_COMMIT_SHA
 cd ..
 
-git clone -b R4_4-RDK https://github.com/rdkcentral/Thunder.git
+git clone --branch  R4_4-RDK https://github.com/rdkcentral/Thunder.git
 cd Thunder
 git checkout $THUNDER_COMMIT_SHA
 cd ..
