2c74472e216434a6eb74d7ce33b98de559e8c49b
srinibas15 <113517102+srinibas15@users.noreply.github.com>
2026-08-07 23:33:25 +0530

    RDKEMW-22169:Update build_dependencies.sh

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -39,13 +39,13 @@ cd Thunder
 git checkout $THUNDER_COMMIT_SHA
 cd ..
 
-git clone --branch 4.0.8 https://github.com/rdkcentral/entservices-apis.git
+git clone --branch develop https://github.com/rdkcentral/entservices-apis.git
 
 cd ..
 git clone --branch develop https://github.com/rdkcentral/entservices-helpers.git
 cd "$GITHUB_WORKSPACE"
 
-git clone --branch develop https://github.com/rdkcentral/entservices-testframework.git
+git clone --branch 2.0.0 https://github.com/rdkcentral/entservices-testframework.git
 
 git clone --branch main https://github.com/rdkcentral/networkmanager.git
 
