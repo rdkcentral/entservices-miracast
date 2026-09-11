9d662df10cf46a2c9e62736f05cd983afa464b56
balav08 <54432605+balav08@users.noreply.github.com>
2026-09-11 14:48:14 +0530

    RDKEMW-23188 : Update build_dependencies.sh

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -12,7 +12,7 @@ cd ${GITHUB_WORKSPACE}
 #1. Install Dependencies and packages
 
 apt update
-apt install -y libsqlite3-dev libcurl4-openssl-dev valgrind lcov clang libsystemd-dev libboost-all-dev libwebsocketpp-dev meson libcunit1 libcunit1-dev curl protobuf-compiler-grpc libgrpc-dev libgrpc++-dev libunwind-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev wpa-supplicant
+apt install -y libsqlite3-dev libcurl4-openssl-dev valgrind lcov clang libsystemd-dev libboost-all-dev libwebsocketpp-dev meson libcunit1 libcunit1-dev curl protobuf-compiler-grpc libgrpc-dev libgrpc++-dev libunwind-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libwpa-client-dev
 pip install jsonref
 
 ############################
